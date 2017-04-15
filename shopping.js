8cadecbf1327085fe7ba9c0f8c4d9e421937fa07
Ranjith Reddy <ranjith080216@gmail.com>
2017-04-15 19:22:58 -0400

    shopping cart example

---
+++ shopping.js
@@ -0,0 +1,22 @@
+var myApp = angular
+                   .module("myModule", [])
+                   .controller("myCtrl", function($scope){
+                        var products = ["Apple", "Milk", "Dates"];
+                        $scope.products = products;
+                        $scope.addItem = function(){
+                            $scope.errortext = "";
+                            if(!$scope.addMe){
+                                return;
+                            }
+                            if($scope.products.indexOf($scope.addMe) == -1){
+                                $scope.products.push($scope.addMe);
+                            } else{
+                                $scope.errortext = "The item is already in your shopping list";
+                            }
+                            
+                        }
+                        $scope.removeItem = function(x){
+                            $scope.errortext = "";
+                            $scope.products.splice(x,1);
+                        }
+                   });
